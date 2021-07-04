088f455701f6a32bbfac470db67c88266151a418
Krzysztof <krzysieksz96@gmail.com>
2021-07-04 13:57:36 +0200

    remove unused import

--- src/main/java/com/cloudservices/homework/adapters/generator/exceptions/CannotGenerateProposalUuidException.java
+++ src/main/java/com/cloudservices/homework/adapters/generator/exceptions/CannotGenerateProposalUuidException.java
@@ -4,7 +4,6 @@ import com.cloudservices.homework.domain.model.proposal.exceptions.ProposalExcep
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ResponseStatus(value = INTERNAL_SERVER_ERROR, reason = "Cannot generate proposal UUID")
 public class CannotGenerateProposalUuidException extends ProposalException {
